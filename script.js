de083540bf8c6ba1573ef2a4565786d4c76a5851
Aman <amanvermaspace@gmail.com>
2021-07-31 21:37:04 +0530

    First changes

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Github Token :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
+// const generateButton = document.querySelector(".generate--btn");
+// generateButton.addEventListener("click", function () {
+//   anime({
+//     loop: true,
+//     targets: "div",
+//     translateX: 250,
+//   });
+// });
+
+for (let i = 0; i < document.querySelectorAll("img").length; i++) {
+  document.querySelectorAll("img")[i].classList.add("background");
+}
