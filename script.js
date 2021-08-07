c5078a571a54a12220c1bfc0ff9e2bb66611204a
Aman <amanvermaspace@gmail.com>
2021-08-08 00:30:38 +0530

    display colour hex codes for hue panel beneath them

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// const generateButton = document.querySelector(".generate--btn");
-// generateButton.addEventListener("click", function () {
-//   anime({
-//     loop: true,
-//     targets: "div",
-//     translateX: 250,
-//   });
-// });
-
 const generate_button = document.querySelector(".generate-button");
 const divArr = document.querySelectorAll(".main-card-images");
 const innerImages = document.querySelectorAll(".inner-images");
@@ -17,6 +8,8 @@ const div4 = document.querySelector(".div-4");
 const div5 = document.querySelector(".div-5");
 const div6 = document.querySelector(".div-6");
 const values = document.querySelectorAll(".values--container");
+// FOR DISPLAYING HEX CODES
+const names = document.querySelectorAll(".values");
 
 // COLOUR GENERATE FUNCTION
 generate_button.addEventListener("click", function () {
@@ -28,7 +21,60 @@ generate_button.addEventListener("click", function () {
 });
 
 // COLOUR CONVERTER FUNCTION
-function hexToHSL(H) {
+
+// HSL TO HEX
+
+function HSLToHex(h, s, l) {
+  s /= 100;
+  l /= 100;
+
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+    x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+    m = l - c / 2,
+    r = 0,
+    g = 0,
+    b = 0;
+
+  if (0 <= h && h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (60 <= h && h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (240 <= h && h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  } else if (300 <= h && h < 360) {
+    r = c;
+    g = 0;
+    b = x;
+  }
+  // Having obtained RGB, convert channels to hex
+  r = Math.round((r + m) * 255).toString(16);
+  g = Math.round((g + m) * 255).toString(16);
+  b = Math.round((b + m) * 255).toString(16);
+
+  // Prepend 0s, if necessary
+  if (r.length == 1) r = "0" + r;
+  if (g.length == 1) g = "0" + g;
+  if (b.length == 1) b = "0" + b;
+
+  return "#" + r + g + b;
+}
+
+// HEX TO HSL: FOR LUMOSITY
+function lumosityCalc(H) {
   // Convert hex to RGB first
   let r = 0,
     g = 0,
@@ -60,14 +106,95 @@ function hexToHSL(H) {
 
   l = (cmax + cmin) / 2;
   s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-  s = +(s * 100).toFixed(0);
-  l = +(l * 100).toFixed(0);
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
 
   // return "hsl(" + h + "," + s + "%," + l + "%)";
   // return `hsl(${h}, ${s}%, ${l}%)`;
   return l;
 }
-console.log(hexToHSL(Math.floor(Math.random() * 16777215).toString(16)));
+// HEX TO HSL: FOR SATURATION
+function saturationCalc(H) {
+  // Convert hex to RGB first
+  let r = 0,
+    g = 0,
+    b = 0;
+  if (H.length >= 3) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r, g, b),
+    cmax = Math.max(r, g, b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+
+  if (delta == 0) h = 0;
+  else if (cmax == r) h = ((g - b) / delta) % 6;
+  else if (cmax == g) h = (b - r) / delta + 2;
+  else h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0) h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  // return "hsl(" + h + "," + s + "%," + l + "%)";
+  // return `hsl(${h}, ${s}%, ${l}%)`;
+  return s;
+}
+
+// HEX TO HSL: FOR HUE
+function hueCalc(H) {
+  // Convert hex to RGB first
+  let r = 0,
+    g = 0,
+    b = 0;
+  if (H.length >= 3) {
+    r = "0x" + H[1] + H[1];
+    g = "0x" + H[2] + H[2];
+    b = "0x" + H[3] + H[3];
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r, g, b),
+    cmax = Math.max(r, g, b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+
+  if (delta == 0) h = 0;
+  else if (cmax == r) h = ((g - b) / delta) % 6;
+  else if (cmax == g) h = (b - r) / delta + 2;
+  else h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0) h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  // return "hsl(" + h + "," + s + "%," + l + "%)";
+  // return `hsl(${h}, ${s}%, ${l}%)`;
+  return h;
+}
+
 // COLOUR RETURN EFFECT
 document.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
@@ -90,16 +217,24 @@ div1.addEventListener("click", function () {
   div1.classList.toggle("radius-change1");
   div1.classList.toggle("motion");
 
+  // HEX TO HSL
   var hexCodedColor = innerImages[0].textContent;
   var codedColor = hexCodedColor.split("#"); // splits the hex code into an array separated by a '#' so that the number can be passed into the hexToHSL function.
-  var lumosity = hexToHSL(codedColor[1]);
+  var lumosity = lumosityCalc(codedColor[1]);
+  var saturation = saturationCalc(codedColor[1]);
+  var hue = hueCalc(codedColor[1]);
+
+  console.log(names[1].textContent);
+  console.log(HSLToHex(hue, saturation, lumosity));
+  console.log(saturation);
+  console.log(lumosity);
+  console.log(hue);
+
   for (let i = 0, hue = 0; i < 15; i++) {
+    names[i].textContent = HSLToHex(hue, 100, lumosity);
     values[i].style.backgroundColor = `hsl(${hue}, 100%, ${lumosity}%)`;
     hue += 24;
   }
-  // HEX CODE OF FIRST COLOUR IN COLOUR PALETTE
-
-  // CONVERTING THE HEX CODE TO HSL
 });
 
 div2.addEventListener("click", function () {
@@ -118,34 +253,3 @@ div6.addEventListener("click", function () {
   div6.classList.toggle("radius-change2");
   div6.classList.toggle("motion");
 });
-
-function hConvert(colour1) {
-  var colour1 = divArr[0].firstElementChild.textContent;
-  let r = 0,
-    g = 0,
-    b = 0;
-  if (colour1.length >= 3) {
-    r = "0x" + colour1[1] + colour1[1];
-    g = "0x" + colour1[2] + colour1[2];
-    b = "0x" + colour1[3] + colour1[3];
-  }
-  r /= 255;
-  g /= 255;
-  b /= 255;
-  let cmin = Math.min(r, g, b),
-    cmax = Math.max(r, g, b),
-    delta = cmax - cmin,
-    h = 0;
-
-  if (delta == 0) h = 0;
-  else if (cmax == r) h = ((g - b) / delta) % 6;
-  else if (cmax == g) h = (b - r) / delta + 2;
-  else h = (r - g) / delta + 4;
-
-  h = Math.round(h * 60);
-
-  if (h < 0) h += 360;
-
-  return h;
-}
-console.log(hConvert());
